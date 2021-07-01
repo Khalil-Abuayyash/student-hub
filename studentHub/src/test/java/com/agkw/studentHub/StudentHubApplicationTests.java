4232b8f79637e950e2a60c66aec3c3c58151b454
khalil <khalilayyash@gmail.com>
2021-07-01 16:42:54 +0300

    initial commit

---
+++ studentHub/src/test/java/com/agkw/studentHub/StudentHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agkw.studentHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
